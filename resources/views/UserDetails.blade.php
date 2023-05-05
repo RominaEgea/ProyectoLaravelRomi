04055ee4d100b5620bc2037cce4be1015ef1ccd5
regea <cirominaegea@gmail.com>
2023-05-05 12:28:46 -0300

    actualizando

---
+++ resources/views/UserDetails.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User Details</title>
+</head>
+<body style="display: grid; place-items:centrer; height:100vh; width:100vw">
+    <h1>The id of the clicket user is: {{$id}} and its name is {{$username}}</h1>
+</body>
+</html>
